e13d71cd2d4f0b4d1bbb27d8489f323e0dded180
Krevchik Angelina <31546115+Angelina151612@users.noreply.github.com>
2020-11-09 00:23:56 +0300

    Update test04.py

--- Homework1/Task04/test04.py
+++ Homework1/Task04/test04.py
@@ -1,12 +1,12 @@
-import pytest
 from typing import List
 
+import pytest
 
 from task04 import check_sum_of_four
 
 
 @pytest.mark.parametrize(
-    ["value1", "value2", "value3", "value4", "expected_result"],
+    ("value1", "value2", "value3", "value4", "expected_result"),
     [
         ([0, 0, 2, 3], [0, 0, 7, 7], [0, 5, 7, 7], [0, 6, 7, 7], 4),
         ([1, 0, 2, 3], [1, 0, 7, 7], [8, 5, 7, 7], [2, 6, 7, 7], 0),
