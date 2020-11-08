eb2c0e11213d1f94f4a292583dc9dc81f9811bf0
Angelina151612 <angelina151612@gmail.com>
2020-11-08 22:49:25 +0300

    fix HW2 Task3

--- Homework2/Task03/task03.py
+++ Homework2/Task03/task03.py
@@ -1,22 +1,19 @@
+"""
+Write a function that takes K lists as arguments and returns all possible
+lists of K items where the first element is from the first list,
+the second is from the second and so one.
+You may assume that that every list contain at least one element
+Example:
+assert combinations([1, 2], [3, 4]) == [
+    [1, 3],
+    [1, 4],
+    [2, 3],
+    [2, 4],
+]
+"""
 import itertools
 from typing import Any, List
 
 
 def combinations(*args: List[Any]) -> List[List]:
-    """
-    Write a function that takes K lists as arguments and returns all possible
-    lists of K items where the first element is from the first list,
-    the second is from the second and so one.
-    You may assume that that every list contain at least one element
-    Example:
-    assert combinations([1, 2], [3, 4]) == [
-        [1, 3],
-        [1, 4],
-        [2, 3],
-        [2, 4],
-    ]
-    """
-    all_comb = []
-    for comb in itertools.product(*args):
-        all_comb.append(list(comb))
-    return all_comb
+    return [list(comb) for comb in itertools.product(*args)]
