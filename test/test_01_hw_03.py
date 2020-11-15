7d8e06ac5ece5e55b2f6de33f72c4d38174ebfe0
Angelina151612 <angelina151612@gmail.com>
2020-11-16 02:00:55 +0300

    fix HW

--- test/test_01_hw_03.py
+++ test/test_01_hw_03.py
@@ -4,10 +4,12 @@ from hw.hw_03_task_01 import cache
 
 
 def test_cache():
-    m = mock.Mock()
-    n = m
-    m = cache(times=2)(m)
-    for _ in range(4):
-        m()
-
-    assert n.call_count == 2
+    my_mock = mock.Mock()
+    mock_copy = my_mock
+    my_mock = cache(times=2)(my_mock)
+    my_mock()
+    my_mock()
+    my_mock()
+    assert mock_copy.call_count == 1
+    my_mock()
+    assert mock_copy.call_count == 2
