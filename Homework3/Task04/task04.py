8990ffdf4fb9b15c65ec91edf0cffe1e4934aba5
Angelina151612 <angelina151612@gmail.com>
2020-11-15 04:11:24 +0300

    HW3 Task04

--- Homework3/Task04/task04.py
+++ Homework3/Task04/task04.py
@@ -1,5 +1,4 @@
 def is_armstrong(number: int) -> bool:
-    digits = list(map(int, str(number)))
-    power = len(digits)
-    res = sum(list(map(lambda x: x ** power, digits)))
+    power = len(str(number))
+    res = sum(x ** power for x in map(int, str(number)))
     return number == res
