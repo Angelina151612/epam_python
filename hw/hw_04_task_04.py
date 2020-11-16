3fdbf7d41dc4ab71581b479fe42b6689be45473e
Angelina151612 <angelina151612@gmail.com>
2020-11-16 23:25:00 +0300

    HW4 Task04

---
+++ hw/hw_04_task_04.py
@@ -0,0 +1,45 @@
+from typing import List
+
+
+# flake8: noqa: CCR001
+def fizzbuzz(n: int) -> List[str]:
+    """
+    Function takes a number N as an input and returns N FizzBuzz numbers.
+
+    Instruction:
+     - Install Python 3.8 (https://www.python.org/downloads/)
+     - Install pytest `pip install pytest`
+     - Clone the repository <path your repository>
+     - Checkout branch <your branch>
+     - Open terminal
+     - Run python hw/hw_04_task_04.py -v
+
+    >>> fizzbuzz(5)
+    ['1', '2', 'Fizz', '4', 'Buzz']
+    >>> fizzbuzz(1.5)
+    Traceback (most recent call last):
+        ...
+    TypeError: Wrong number!
+    """
+    try:
+        res = []
+        for i in range(1, n + 1):
+            div_3 = i % 3
+            div_5 = i % 5
+            if div_3 == 0 and div_5 == 0:
+                res.append("Fizz Buzz")
+            elif div_3 == 0:
+                res.append("Fizz")
+            elif div_5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+        return res
+    except TypeError:
+        raise TypeError("Wrong number!")
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
