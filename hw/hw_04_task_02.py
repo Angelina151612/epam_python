35b6a990a3cf24d347d253f5e5742070195f2dd8
Angelina151612 <angelina151612@gmail.com>
2020-11-18 00:10:16 +0300

    corrected hw

--- hw/hw_04_task_02.py
+++ hw/hw_04_task_02.py
@@ -5,8 +5,9 @@ import urllib.request
 def count_dots_on_i(url: str) -> int:
     try:
         page = urllib.request.urlopen(url)
+
         data = page.read().decode()
         page.close()
         return sum(1 for symb in data if symb == "i")
-    except ValueError:
+    except:
         raise ValueError("Unreachable {url}")
