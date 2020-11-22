ad88751dcd590f9fa032f784e8d2fb902632c1e7
Angelina151612 <angelina151612@gmail.com>
2020-11-23 00:01:00 +0300

    fixed test

--- test/test_02_hw_03.py
+++ test/test_02_hw_03.py
@@ -1,7 +1,5 @@
-from hw.hw_03_task_02 import fast_calculate, slow_calculate
+from hw.hw_03_task_02 import fast_calculate
 
 
 def test_fast_calculate():
-    res_slow = sum(slow_calculate(i) for i in range(501))
-    res_fast = fast_calculate()
-    assert res_slow == res_fast
+    assert fast_calculate() == 1025932
