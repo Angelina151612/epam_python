c80f0b82a1406fd895b85a52556364a7d0a733ae
Angelina151612 <angelina151612@gmail.com>
2020-12-13 00:23:27 +0300

    fixed hw

--- hw/hw_08_task_02.py
+++ hw/hw_08_task_02.py
@@ -1,5 +1,14 @@
 import sqlite3
-from typing import Tuple
+from typing import Callable, Tuple
+
+
+def connect_database(func: Callable) -> sqlite3.Cursor:
+    def wrapper(self, *args, **kwargs):
+        with sqlite3.connect(self.database_name) as conn:
+            cursor = conn.cursor()
+            return func(self, cursor, *args, **kwargs)
+
+    return wrapper
 
 
 # flake8: noqa: S608
@@ -8,27 +17,26 @@ class TableData:
         self.database_name = database_name
         self.table_name = table_name
 
-    @staticmethod
-    def connect_database(database_name: str) -> sqlite3.Cursor:
-        conn = sqlite3.connect(database_name)
-        return conn.cursor()
-
-    def __len__(self) -> int:
-        cursor = TableData.connect_database(self.database_name)
-        cursor.execute("SELECT COUNT(*) from " + self.table_name)
+    @connect_database
+    def __len__(self, cursor) -> int:
+        cursor.execute(f"SELECT COUNT(*) from {self.table_name}")
         return cursor.fetchone()[0]
 
-    def __getitem__(self, item: str) -> Tuple:
-        cursor = TableData.connect_database(self.database_name)
-        cursor.execute("SELECT * from " + self.table_name + " WHERE name =?", (item,))
+    @connect_database
+    def __getitem__(self, cursor, item: str) -> Tuple:
+        cursor.execute(
+            f"SELECT * from  {self.table_name} WHERE name = :item", {"item": item}
+        )
         return cursor.fetchone()
 
-    def __contains__(self, item: str) -> bool:
-        cursor = TableData.connect_database(self.database_name)
-        cursor.execute("SELECT * from " + self.table_name + " WHERE name =?", (item,))
-        return False if cursor.fetchone() is None else True
+    @connect_database
+    def __contains__(self, cursor, item: str) -> bool:
+        cursor.execute(
+            f"SELECT * from {self.table_name} WHERE name =:item", {"item": item}
+        )
+        return cursor.fetchone()
 
-    def __iter__(self) -> Tuple:
-        cursor = TableData.connect_database(self.database_name)
-        cursor.execute("SELECT * from " + self.table_name)
+    @connect_database
+    def __iter__(self, cursor) -> Tuple:
+        cursor.execute(f"SELECT * from {self.table_name}")
         return cursor
