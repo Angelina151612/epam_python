ff2eabe5eb01c562ac9a3b804a712a92860fd164
Angelina151612 <angelina151612@gmail.com>
2020-12-17 02:42:49 +0300

    HW9T02

---
+++ hw/hw_09_task_02.py
@@ -0,0 +1,28 @@
+"""
+Write a context manager, that suppresses passed exception.
+
+Do it both ways: as a class and as a generator.
+"""
+
+from contextlib import contextmanager
+
+
+# flake8: noqa: R503
+class SuppressorCls:
+    def __init__(self, exception):
+        self.exception = exception
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type == self.exception:
+            return True
+
+
+@contextmanager
+def suppressor_gen(exception):
+    try:
+        yield
+    except exception:
+        pass
---
+++ test/test_02_hw_09.py
@@ -0,0 +1,25 @@
+from hw.hw_09_task_02 import SuppressorCls, suppressor_gen
+
+import pytest
+
+
+def test_class_suppressed_passed_exception():
+    with SuppressorCls(IndexError):
+        [][2]
+
+
+def test_generator_suppressed_passed_exception():
+    with suppressor_gen(IndexError):
+        [][2]
+
+
+def test_class_not_suppressed_exception():
+    with pytest.raises(ZeroDivisionError):  # noqa:
+        with suppressor_gen(IndexError):
+            1 / 0
+
+
+def test_generator_not_suppressed_exception():
+    with pytest.raises(ZeroDivisionError):  # noqa:
+        with SuppressorCls(IndexError):
+            1 / 0
